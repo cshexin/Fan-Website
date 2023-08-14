d8092d99df1d4b2daa0fe7b33de140bf2a4d3541
cshexin <cshexin@outlook.com>
2023-08-14 01:23:11 -0500

    update the package

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 
-app.listen(3001, ()=>{
-    console.log('howdy server')
+const db = require("./models")
+
+db.sequelize.sync().then(()=>{
+    app.listen(3001, ()=>{
+        console.log('howdy server')
+    })
 })
+
