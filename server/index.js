a27401cb789a7ccc3fc99a6576da8be778ef4335
cshexin <cshexin@outlook.com>
2023-08-15 03:03:49 -0500

    update routers

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
-
+app.use(express.json());
 const db = require("./models")
+
+//Router
+const albumRouter = require('./routes/albumsRoute')
+app.use("/albums", albumRouter);
 
 db.sequelize.sync().then(()=>{
     app.listen(3001, ()=>{
