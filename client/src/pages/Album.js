bc95407e211c8c72f3e692f5ce3d7bae39193f75
cshexin <cshexin@outlook.com>
2023-09-15 02:52:55 -0500

    update the gui of album displaying

--- client/src/pages/Album.js
+++ client/src/pages/Album.js
@@ -3,10 +3,12 @@ import axios from 'axios';
 import * as React from 'react';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import Grid from '@mui/material/Grid';
 
 import NavBar from '../config/Nav';
 
 import './Album.css';
+
 
 
 function Album() {
@@ -20,29 +22,30 @@ function Album() {
     }, []);
 
     const navigate = useNavigate(); 
-
-  return (
-    <div className="App">
-      <NavBar/>
-      <div className="albums-container">
-
-        {listOfAlbums.map((value, key) => {
-          return (
-            <div key={key}>
-              <div className="album" onClick={() => { navigate(`/albuminfo/${value.id}`) }}>
-                  <img src={`http://localhost:3001/covers/album/${value.cover}`} alt={`${value.title} cover`} />
-                  <div className="title">{value.title}</div>
-                  <div className="date">{value.released_date}</div>
-              </div>
-            </div>
-          );
-        })}
+    function formatDateToChinese(inputDate) {
+      const parts = inputDate.split('-');
+      if (parts.length !== 3) return inputDate; 
+      return `${parts[0]}年${parts[1]}月${parts[2]}日`;
+  }
+    return (
+      <div className="App">
+        <NavBar/>
+        <Grid className='albums-container' container spacing={3}> {/* Just a sample value, adjust as needed */}
+          {listOfAlbums.map((value, key) => {
+            return (
+              <Grid item xs={12} sm={6} md={4} key={key}> 
+                <div className="album" onClick={() => { navigate(`/albuminfo/${value.id}`) }}>                  
+                    <img src={`http://localhost:3001/covers/album/${value.cover}`} alt={`${value.title} cover`} />
+                    <div className="title">{value.title}</div>
+                    <div className="date">{formatDateToChinese(value.released_date)}</div>
+                </div>
+              </Grid>
+            );
+          })}
+        </Grid>
       </div>
-
-      
-
-    </div>
-  );
+    );
+    
 }
 
 export default Album;
